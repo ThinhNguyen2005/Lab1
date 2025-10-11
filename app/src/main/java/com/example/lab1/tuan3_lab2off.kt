82bf01fc13bac2c8ea92ab24ddabe7f543f63978
ThinhNguyen2005 <nguyenthinhk52005@gmail.com>
2025-10-11 09:17:01 +0700

    bài tập tuần 3 tại lớp

--- app/src/main/java/com/example/lab1/tuan3_lab2off.kt
+++ app/src/main/java/com/example/lab1/tuan3_lab2off.kt
@@ -1,14 +1,12 @@
 package com.example.lab1
 
 
-import android.os.Message
-import androidx.compose.foundation.Image // <-- 1. Import Composable Image
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape // <-- Import CircleShape để bo tròn ảnh
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
@@ -16,10 +14,8 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip // <-- Import clip để cắt ảnh
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale // <-- Import để chỉnh cách ảnh hiển thị
-import androidx.compose.ui.res.painterResource // <-- 2. Import painterResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
@@ -37,7 +33,6 @@ fun tuan3_lab2off() {
     ) {
 
 
-        // --- THÊM HÌNH ẢNH TẠI ĐÂY ---
         Image(
             painter = painterResource(id = R.drawable.avatar),
             contentDescription = "Ảnh đại diện"
